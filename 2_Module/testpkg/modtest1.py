33b562da9d560b32933d4e36c7252036e8d9a0e6
Em Ekdahl - Data Science <emekdahl-datascience@Emilys-MBP-Loaner.local>
2020-08-25 07:31:48 -0500

    packages

---
+++ 2_Module/testpkg/modtest1.py
@@ -0,0 +1,5 @@
+my_tuple = ('yes', 'no')
+my_boolean = True
+
+def my_func1():
+    print('Executed my_func1() inside testpkg')
---
+++ 2_Module/testpkg/modtest2.py
@@ -0,0 +1,5 @@
+my_set = {1,2}
+my_boolean = "One package to rule them all."
+
+def my_func2():
+    print('Executed my_func2() inside testpkg')
